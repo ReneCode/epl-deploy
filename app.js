e20f86d6b11b5b0acb61da72abbd0d0df8b17db3
rene <harryloveslearning@googlemail.com>
2016-06-26 12:11:07 +0200

    port 3333

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 var app = require('./server');
-var port = process.env.PORT || 3030;
+var port = process.env.PORT || 3333;
 app().listen(port, function() {
 	console.log("Server listen on port:", port);
 });
