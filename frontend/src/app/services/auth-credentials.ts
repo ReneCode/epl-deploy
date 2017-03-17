a65548ed58c348ec2d031030bb398dfba048faba
rene_mbp <harryloveslearning@googlemail.com>
2017-03-17 11:51:36 +0100

    changed

--- frontend/src/app/services/auth-credentials.ts
+++ frontend/src/app/services/auth-credentials.ts
@@ -1,7 +1,7 @@
 // from https://manage.auth0.com
 
 export const authCredentials = {
-  clientId: '----',
-  domain: '----'
+  clientId: 'pRUSiTOfYn0CaXAmuRP2OQv5ePZd1aq9',
+  domain: 'eplan.eu.auth0.com'
 };
 
